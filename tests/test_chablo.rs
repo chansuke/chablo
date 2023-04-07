d36972602963e370b4793138c2e87fc87648087a
chansuke <moonset20@gmail.com>
2023-04-07 15:55:25 +0900

    fix: restore windows os case

--- tests/test_chablo.rs
+++ tests/test_chablo.rs
@@ -18,9 +18,14 @@ fn test_chablo_error() -> Result<(), Box<dyn Error>> {
     let mut bin = bin();
     bin.arg("unknown_command");
 
-    let msg = "error: unrecognized subcommand 'unknown_command'\n\nUsage: chablo <COMMAND>\n\nFor more information, try '--help'.";
+    let msg = if cfg!(windows) {
+        "error: unrecognized subcommand 'unknown_command'\n\nUsage: chablo.exe <COMMAND>\n\nFor more information, try '--help'."
+    } else {
+        "error: unrecognized subcommand 'unknown_command'\n\nUsage: chablo <COMMAND>\n\nFor more information, try '--help'."
+    };
 
     bin.assert().failure().stderr(predicate::str::contains(msg));
+
     Ok(())
 }
 
